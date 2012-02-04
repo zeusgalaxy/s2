19c86b96d6d9b36f29c819af239c816216ecd158
Dennis Faust <dennis@dennisfaust.com>
2012-02-03 20:17:34 -0800

    First whack at using the report DB.

--- app/reports/WorkoutLocation.scala
+++ app/reports/WorkoutLocation.scala
@@ -7,9 +7,24 @@ import play.api.Play.current
 import anorm._
 import anorm.SqlParser._
 
-case class WorkoutLocation(id: Pk[Long] = NotAssigned, companyName: String, clubName: String,  city: String, 
-                           machineCnt: Long,
-                           workoutCnt: java.math.BigDecimal
+// available columns : location.company_id, location.location_id, location.company_name, location.club_name,
+// location.club_city, location.club_zip,
+// workout_day_sum.machine_id, workout_day_sum.location_id, workout_day_sum.day_int, workout_day_sum.duration_avg,
+// workout_day_sum.duration_tot, workout_day_sum.duration_pct, workout_day_sum.workout_cnt,
+// workout_day_sum.e, workout_day_sum.workout_uniq_user, workout_day_sum.calories_avg,
+// workout_day_sum.calories_tot, workout_day_sum.distance_avg, workout_day_sum.distance_tot,
+// workout_day_sum.distance_unit, workout_day_sum.speed_avg, workout_day_sum.speed_min, workout_day_sum.speed_max,
+// workout_day_sum.speed_unit, workout_day_sum.vertical_avg, workout_day_sum.vertical_unit,
+// workout_day_sum.used_wkt_program_pct,
+// time_dim.day_int, time_dim.year_number, time_dim.month_number, time_dim.day_of_year_number,
+// time_dim.day_of_month_number, time_dim.day_of_week_number, time_dim.week_of_year_number, time_dim.day_name,
+// time_dim.month_name, time_dim.quarter_number, time_dim.quarter_name, time_dim.year_quarter_name,
+// time_dim.weekend_ind, time_dim.days_in_month_qty, time_dim.day_int_1, time_dim.day_desc, time_dim.week_sk,
+// time_dim.day_date, time_dim.week_name, time_dim.week_of_month_number, time_dim.week_of_month_name,
+// time_dim.year_sk, time_dim.month_sk, time_dim.quarter_sk, time_dim.day_of_week_sort_name, time_dim.year_sort_number
+
+case class WorkoutLocation(id: Pk[Long] = NotAssigned, clubName: String,
+                           screens: Long, workoutCnt: java.math.BigDecimal
                             )
 
 object WorkoutLocation {
@@ -23,14 +38,12 @@ object WorkoutLocation {
    * Parse a WorkoutLocation from a ResultSet
    */
   val simple = {
-    get[Pk[Long]]("tmp_workout_report.location_id") ~
-      get[String]("tmp_workout_report.company_name") ~
-      get[String]("tmp_workout_report.club_name") ~
-      get[String]("tmp_workout_report.club_city") ~
-      get[Long]("tmp_workout_report.machine_cnt") ~
-    get[java.math.BigDecimal]  ("tmp_workout_report.workout_cnt") map {
-      case id~companyName~clubName~city~machineCnt~workoutCnt =>
-        WorkoutLocation(id, companyName, clubName, city, machineCnt, workoutCnt )
+    get[Pk[Long]]("location.location_id") ~
+      get[String]("location.club_name") ~
+      get[Long]("screens") ~
+      get[java.math.BigDecimal]("wo_cnt") map {
+      case id~clubName~screens~workoutCnt =>
+        WorkoutLocation(id, clubName, screens, workoutCnt )
     }
   }
 
@@ -62,9 +75,22 @@ object WorkoutLocation {
 
       val woL = SQL(
         """
-          select * from tmp_workout_report
-          where club_name like {filter}
-          and company_name = {companyLimit}
+          select l.location_id, l.club_name,
+          count(distinct(s.machine_id)) as screens,
+          sum(s.workout_cnt) as wo_cnt,
+          sum(s.workout_regisr) as reg_wo,
+          sum(s.workout_cnt) / sum(s.workout_regisr) as perc_reg,
+          count(distinct(s.machine_id)) / sum(s.workout_cnt) as wo_screen,
+          count(distinct(s.day_int)) as days,
+          sum(s.workout_cnt) / count(distinct(s.day_int)) as wo_screen_day,
+          sum(s.duration_tot) / sum(s.workout_cnt) as dur_avg,
+          sum(s.duration_tot) / 60 as dur_tot,
+          count(*)
+          from  workout_day_sum s
+          join location l on s.location_id = l.location_id
+          join time_dim t on t.day_int = s.day_int
+          where company_name = {companyLimit}
+          group by l.location_id
           order by {orderBy}
           limit {pageSize} offset {offset}
         """
@@ -78,7 +104,9 @@ object WorkoutLocation {
 
       val totalRows = SQL(
         """
-          select count(*) from tmp_workout_report
+          select count(*) from  workout_day_sum s
+          join location l on s.location_id = l.location_id
+          join time_dim t on t.day_int = s.day_int
           where club_name like {filter}
           and company_name = {companyLimit}
         """
