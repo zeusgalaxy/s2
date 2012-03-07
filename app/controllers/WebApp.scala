f450ed8084afbbf8d48d0ee203aba5a9c3ac565a
Dennis Faust <dennis@dennisfaust.com>
2012-03-06 21:47:04 -0800

    Barely got the tiniest, atomic, user edit capability going.

--- app/controllers/WebApp.scala
+++ app/controllers/WebApp.scala
@@ -1,7 +1,15 @@
 package controllers
 
+import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import anorm._
+
 import views._
+import models._
+
 
 object WebApp extends Controller with Secured {
 
@@ -12,5 +20,57 @@ object WebApp extends Controller with Secured {
   def testLogin = IsAuthenticated("/testLogin", user => implicit request =>
     Ok(html.testLogin(user, "test string"))
   )
+
+
+  /*
+  *
+  *  User CRUD controllers, form etc.
+  *        case class User(id: Long = 0, firstName: String="", lastName: String="", password: String="", email: String="",
+                compId: Long=0,  oemId: Option[Long]=None, adId: Option[Long]=None  )
+   */
+
+  val userForm: Form[User] = Form(
+    mapping(
+      "firstName" -> text
+    ){ (firstName) => User(0,firstName,"","","") }
+     {  user => Some(user.firstName ) }
+  )
+
+  def userEdit = IsAuthenticated("/userEdit", user => implicit request =>
+    Ok(html.userEdit(user, userForm))
+  )
+
+//    def userEdit = Action {
+//      Ok(html.userEdit.form(userForm));
+//    }
+
+  /**
+   * Handle form submission.
+   */
+  def userSubmit = IsAuthenticated("/userEdit", user => implicit request =>
+    userForm.bindFromRequest.fold(
+      errors => BadRequest(html.userEdit(user, errors)),
+      user =>  Redirect(routes.WebApp.index).flashing(
+        "success" -> ("User information updated: "+user.toString)
+      )
+        // Ok(html.userEdit(user, userForm))
+    )
+  )
+
+  //  "id" -> Long,
+  //    (
+//      (id, firstName ) => User(id, firstName ),
+//      (user: User) => (user.firstName)
+//     )
+//
+  //      "lastName" ->  text,
+  //      "password" -> nonEmptyText (6),
+  //      "email" -> nonEmptyText()
+  //      "compId" -> ignored (Long),
+  //      "oemId" -> ignored(Option(Long)),
+  //      "adId" -> ignored(Option(Long))
+      //(User.apply)(User.unapply)
+
+
 
 }
