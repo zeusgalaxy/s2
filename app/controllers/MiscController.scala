252fe4315c33467a59768b1fab5ca57c04836e3a
Dennis Faust <dennis@dennisfaust.com>
2012-03-29 18:35:39 -0700

    Clean up the controllers a bit.

--- app/controllers/MiscController.scala
+++ app/controllers/MiscController.scala
@@ -37,46 +37,56 @@ object MiscController extends Controller {
                compId: Long=0,  oemId: Option[Long]=None, adId: Option[Long]=None  )
   */
 
-
-  val userForm= Form(
+  /** userForm
+   * This is a special form to user for user editing since we don't want to use
+   * the full User case class.
+   */
+  val userForm = Form(
     mapping(
       "firstName" -> optional(text),
       "lastName" -> optional(text),
       "email" -> email,
       "password" -> optional(text)
-    )  (UserEdit.apply)(UserEdit.unapply)
+    )(UserEdit.apply)(UserEdit.unapply)
   )
 
+  /** userEdit
+   * Use the passed in ID value to get the user from the DB. Present that info in the form above.
+   * @param id - User ID
+   */
   def userEdit(id: Long) = IfCanUpdate(tgUsers) {
     implicit request =>
-      User.findById(id) match {
-        case Some(u) => {
-          val uE = UserEdit(u.firstName, u.lastName, u.email, None)
-          Ok(html.userEdit(id, userForm.fill(uE)))
-        }
-        case _ => Redirect(routes.MiscController.userList()).flashing("failure" -> ("An error occured."))
-      }
+      User.findById(id).map(user => {
+        Ok(html.userEdit(id, userForm.fill(UserEdit(user.firstName, user.lastName, user.email, None))))
+      }).getOrElse(Redirect(routes.MiscController.userList()).flashing("failure" -> ("An error occured.")))
   }
 
-  /**
+  /** userSubmit
    * Handle form submission.
+   * @param id User ID
    */
   def userSubmit(id: Long) = IfCanUpdate(tgUsers) {
     implicit request => {
       userForm.bindFromRequest.fold(
         formErrors => BadRequest(html.userEdit(id, formErrors)),
-        uE => {
-          User.update(id, uE) match {
+        uE => User.update(id, uE)
+          match {
             case 1 =>
-              Redirect(routes.MiscController.userList()).flashing("success" -> ("User: "+uE.email+" updated."))
+              Redirect(routes.MiscController.userList()).flashing("success" -> ("User: " + uE.email + " updated."))
             case _ =>
               Redirect(routes.MiscController.userList()).flashing("failure" -> ("An error occured."))
           }
-        }
       )
     }
   }
 
+  /**userList
+   *
+   * Display a page by page listing of the the admin users
+   * @param page - which page of paginated results to display
+   * @param orderBy - column to sort by
+   * @param filter - partial last name to use as a match string
+   */
   def userList(page: Int, orderBy: Int, filter: String) = IfCanRead(tgUsers) {
     implicit request =>
       Ok(html.userList(
