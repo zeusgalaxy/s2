33b53cb58e193d00e69b2eca07b5a462453e7a50
Dennis Faust <dennis@dennisfaust.com>
2012-04-25 11:16:30 -0700

    Add a status controller and model for external pinging.

---
+++ app/controllers/StatusController.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import anorm._
+
+import views._
+import models._
+import security._
+import org.joda.time.DateTime
+
+
+object StatusController extends Controller with StatusDao {
+  /**
+   * Return the application status to external monitors. If the return string contains "ok"
+   * then all is well in the application. Add any tests or checks you want in the model or below
+   * in the controller
+   * @return
+   */
+  def status = Action { Ok(if (checkAppStatus) "**** ok ****" else "**** fail ****" ) }
+}
---
+++ app/models/Status.scala
@@ -0,0 +1,27 @@
+package models
+
+import play.api.db._
+import play.api.Play.current
+
+import anorm._
+import utils._
+
+case class Status()
+
+/**
+ * DAO for checking the status of the application
+ * add any checks here
+ */
+trait StatusDao {
+
+  def checkAppStatus: Boolean = checkDB("s2") && checkDB("report") && checkDB()
+
+  def checkDB( db : String = "default" ): Boolean = {
+    implicit val loc = VL("status.checkDB")
+
+    vld {
+        DB.withConnection(db) { implicit connection => SQL("Select 1").execute() }
+    }.error.fold(e => false, s => s )
+  }
+
+}
