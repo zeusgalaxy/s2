3ce920fdb933b4de862ac9b62d58c460646d5bc5
Dennis Faust <dennis@dennisfaust.com>
2012-04-26 12:46:06 -0700

    add version number to status check.

--- app/controllers/StatusController.scala
+++ app/controllers/StatusController.scala
@@ -2,15 +2,9 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import play.api.data._
-import play.api.data.Forms._
+import play.api.Play.current
 
-import anorm._
-
-import views._
-import models._
-import security._
-import org.joda.time.DateTime
+import models.StatusDao
 
 
 object StatusController extends Controller with StatusDao {
@@ -18,7 +12,13 @@ object StatusController extends Controller with StatusDao {
    * Return the application status to external monitors. If the return string contains "ok"
    * then all is well in the application. Add any tests or checks you want in the model or below
    * in the controller
+   *
+   * import play.api.Play.current
+   * play.api.Play.configuration.keys.toString gets all app settings
+   * play.api.Play.configuration.getString("mrj.build").getOrElse("")
+   *
    * @return
    */
-  def status = Action { Ok(if (checkAppStatus) "**** ok ****\n" else "**** fail ****\n" ) }
-}
+  def status = Action {
+     Ok( "Version: "+ play.api.Play.configuration.getString("appVersion").getOrElse("") + " : "+ (if (checkAppStatus) "**** ok ****\n" else "**** fail ****\n")  ) }
+  }
